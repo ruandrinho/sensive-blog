0ad9226d627f648315fd9abd5c76ec29c6cc7ca0
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-07 11:21:35 +0500

    Fix error

--- blog/views.py
+++ blog/views.py
@@ -34,7 +34,7 @@ def serialize_tag(tag):
 def index(request):
 
     posts = Post.objects.all()
-    popular_posts = sorted(posts, get_likes_count)
+    popular_posts = sorted(posts, key=get_likes_count)
     most_popular_posts = popular_posts[:-5:]  # TODO. Как это посчитать?
 
     fresh_posts = Post.objects.order_by('published_at')
