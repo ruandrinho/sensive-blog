fa75351cc4d798bf95f36402003338e87551fddd
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-07 15:49:41 +0500

    Reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -49,10 +49,11 @@ def serialize_tag(tag):
 def index(request):
 
     most_popular_posts = Post.objects.popular().\
-        prefetch_related('author')[:5].\
+        prefetch_related('author').prefetch_related('tags')[:5].\
         fetch_with_comments_count()
 
     fresh_posts = Post.objects.prefetch_related('author').\
+        prefetch_related('tags').\
         annotate(comments_count=Count('comments')).order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
